7896a6c1669035eb64246809cf23ebd0e5270055
ricardohsmello <ricardohsmello@gmail.com>
2023-02-15 08:55:09 -0300

    adjusts

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
 
-    public static void main(String[] args) {
+ public static void main(String[] args) {
 
         BinaryTree tree = new BinaryTree();
         tree.add(12);
-        tree.add(2);
-        tree.add(8);
         tree.add(4);
-        tree.add(16);
+        tree.add(8);
+        tree.add(2);
         tree.add(6);
-
+        tree.add(16);
 
         System.out.println("#printing inOrder#\n");
         tree.inOrder(tree.getRoot());
@@ -17,6 +16,7 @@ public class Main {
         tree.preOrder(tree.getRoot());
         System.out.println("\n#printing postOrder#\n");
         tree.postOrder(tree.getRoot());
+
 
     }
 }
